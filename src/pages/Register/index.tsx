ea9f9279b9b597af1d7c08f6818fe2717e142122
gustas01 <gustasboyevolution@hotmail.com>
2023-03-08 13:43:20 -0300

    montando cadastro de usuários

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import isEmail from 'validator/es/lib/isEmail';
+import axios from "../../services/axios";
 import { Container } from "../../styles/GlobalStyles";
+import { Form } from "./styled";
 
 export default function Register(): JSX.Element{
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate()
+
+  async function handleSubmit(e: React.FormEvent){
+    e.preventDefault()
+    
+    let formErros = false;
+
+    if(name.length < 3 || name.length > 255){
+      formErros = true;
+      toast.error('O nome deve ter entre 3 e 255 caracteres')
+    }
+
+    if(password.length < 6 || password.length > 50){
+      formErros = true;
+      toast.error('A senha deve ter entre 6 e 50 caracteres')
+    }
+    
+    if (!((/[A-Z]/).test(password) && ((/[a-z]/).test(password)))){
+      formErros = true;
+      toast.error('A senha deve ter pelo menos 1 letra maiúscula e 1 minúscula');
+    }
+
+    if(!isEmail(email)){
+      formErros = true;
+      toast.error('E-mail inválido')
+    }
+
+
+    if(formErros) return
+
+    try{
+      await axios.post('/users', {name, email, password})
+   
+      toast.success('Usuário cadastrado com sucesso!')
+      navigate('/login')
+      
+    }catch(e: any){
+      const errors = (e.response?.data?.errors) || [];
+      
+      errors.map((error: any) => toast.error(error))
+    }
+
+  }
+
   return (
     <Container>
-    <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Nome:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='Seu nome'/>
+        </label>
+        
+        <label htmlFor="email">
+          E-mail:
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder='Seu email'/>
+        </label>
+        
+        <label htmlFor="password">
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder='Sua senha'/>
+        </label>
+
+        <button type="submit">Criar minha conta</button>
+      </Form>
     </Container>
   )
 }
--- src/pages/Register/styled.tsx
+++ src/pages/Register/styled.tsx
@@ -1,6 +1,29 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
 type Props = {}
 
 
-export const Title = styled.h1 <Props>``
+export const Form = styled.form <Props>`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  label{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  input{
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+
+    &:focus{
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`
